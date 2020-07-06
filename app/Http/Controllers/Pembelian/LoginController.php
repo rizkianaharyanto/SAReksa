29946a68b8d7ee62cc6fe9903db8dc4e20b1aa34
rizkianaharyanto <aharyanto93@gmail.com>
2020-07-07 01:27:03 +0700

    udah semua

--- app/Http/Controllers/Pembelian/LoginController.php
+++ app/Http/Controllers/Pembelian/LoginController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers\Pembelian;
 
 use App\Http\Controllers\Controller;
+use App\Pembelian\Faktur;
+use App\Pembelian\Hutang;
+use App\Pembelian\Pemasok;
+use App\Pembelian\Pemesanan;
+use App\Pembelian\Penerimaan;
+use App\Pembelian\Pengirim;
+use App\Pembelian\Permintaan;
+use App\Pembelian\Retur;
 use Illuminate\Http\Request;
 use App\Services\Stock\AuthService;
+use App\Stock\Barang;
+use App\Stock\Gudang;
 
 class LoginController extends Controller
 {
@@ -18,6 +28,25 @@ class LoginController extends Controller
     public function index()
     {
         return view('pembelian.login');
+    }
+
+    public function dashboard()
+    {
+        $pemasoks = Pemasok::count();
+        $pengirims = Pengirim::count();
+        $barangs = Barang::count();
+        $gudangs = Gudang::count();
+        $hutangs= 0;
+        foreach (Hutang::all() as $hutang) {
+            $hutangs += $hutang->sisa;
+        }
+        return view('pembelian.dashboard', [
+            'pemasoks' => $pemasoks,
+            'barangs' => $barangs,
+            'gudangs' => $gudangs,
+            'hutangs' => $hutangs,
+            'pengirims' => $pengirims
+        ]);
     }
 
     public function login(Request $request)
--- resources/views/pembelian/dashboard.blade.php
+++ resources/views/pembelian/dashboard.blade.php
@@ -1,38 +1,186 @@
-@extends('pembelian.template.template')
+@extends('pembelian.template.templatebaru')
 
 @section('judul', 'dashboard')
 
 @section('halaman', 'Dashboard')
 
 @section('isi')
-<div class="row m-3">
-    <div class="col">
-        <div class="row mb-3">
-            <div class="col card" style="width: 65vw; height:50vh;">
-                <div class="card-body">
 
+<div class="col-xl-3 col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <div class="stat-widget-one">
+                <div class="stat-icon dib"><a href="/pembelian/barangs"><i class="fa fa-database text-danger border-danger"></i></a></div>
+                <div class="stat-content dib">
+                    <div class="stat-text">Barang</div>
+                    <div class="stat-digit">{{$barangs}}</div>
                 </div>
-            </div>
-        </div>
-        <div class="row" style="width: 65vw;">
-            <div class="col card mr-3" style="width: 30vw; height:33vh;">
-                <div class="card-body">
-
-                </div>
-            </div>
-            <div class="col card" style="width: 30vw; height:33vh;">
-                <div class="card-body">
-
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card" style="width: 26vw; height:86vh;">
-            <div class="card-body">
-
             </div>
         </div>
     </div>
 </div>
+<div class="col-xl-3 col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <div class="stat-widget-one">
+                <div class="stat-icon dib"><a href="/pembelian/gudangs"><i class="ti-layout-grid2 text-warning border-warning"></i></a></div>
+                <div class="stat-content dib">
+                    <div class="stat-text">Gudang</div>
+                    <div class="stat-digit">{{$gudangs}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <div class="stat-widget-one">
+                <div class="stat-icon dib"><a href="/pembelian/pengirims"><i class="ti-user text-success border-success"></i></a></div>
+                <div class="stat-content dib">
+                    <div class="stat-text">Pengirim</div>
+                    <div class="stat-digit">{{$pengirims}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-6">
+    <div class="card">
+        <div class="card-body">
+            <div class="stat-widget-one">
+                <div class="stat-icon dib"><a href="/pembelian/pemasoks"><i class="ti-user text-primary border-primary"></i></a></div>
+                <div class="stat-content dib">
+                    <div class="stat-text">Pemasok</div>
+                    <div class="stat-digit">{{$pemasoks}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- pembelian -->
+@if(auth()->user()->role->role_name == 'Admin Pembelian')
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-3">
+        <div class="card-body pb-0">
+            <p class="text-light">Permintaan Penawaran</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/permintaans"><i class="fa fa-envelope-open-text text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-3">
+        <div class="card-body pb-0">
+            <p class="text-light">Pemesanan Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/pemesanans"><i class="fa fa-boxes text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-3">
+        <div class="card-body pb-0">
+            <p class="text-light">Penerimaan Barang</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/penerimaans"><i class="fa fa-shipping-fast text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-3">
+        <div class="card-body pb-0">
+            <p class="text-light">Faktur Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/fakturs"><i class="fa fa-clipboard-check text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+@endif
+
+<!-- retur -->
+@if(auth()->user()->role->role_name == 'Admin Retur Pembelian')
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-1">
+        <div class="card-body pb-0">
+            <p class="text-light">Faktur Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/fakturs"><i class="fa fa-clipboard-check text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-1">
+        <div class="card-body pb-0">
+            <p class="text-light">Retur Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/returs"><i class="fa fa-exchange-alt text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+@endif
+
+<!-- hutang -->
+@if(auth()->user()->role->role_name == 'Admin Utang')
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-4">
+        <div class="card-body pb-0">
+            <p class="text-light">Faktur Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/fakturs"><i class="fa fa-clipboard-check text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-4">
+        <div class="card-body pb-0">
+            <p class="text-light">Hutang</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/hutangs"><i class="fa fa-file-invoice-dollar text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-4">
+        <div class="card-body pb-0">
+            <p class="text-light">Pembayaran Hutang</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/pembayarans"><i class="fa fa-hand-holding-usd text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+@endif
+
+@if(auth()->user()->role->role_name == 'Direksi Perusahaan')
+<div class="col-sm-6 col-lg-3">
+    <div class="card text-white bg-flat-color-2">
+        <div class="card-body pb-0">
+            <p class="text-light">Jurnal Khusus Pembelian</p>
+            <div class="stat-widget-one mb-2 d-flex justify-content-center">
+                <div class="stat-icon dib"><a href="/pembelian/jurnals"><i class="fa fa-file-alt text-light border-light"></i></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/.col-->
+@endif
 @endsection
